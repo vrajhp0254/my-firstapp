850d1eb46827924211b5f7b1f8a9f30f371f3e97
patel_vraj_48808 <vrajhp0254@gmail.com>
2023-12-27 17:33:55 +0530

    new

---
+++ app/page.js
@@ -0,0 +1,78 @@
+"use client";
+import React, { useState } from "react";
+
+function page() {
+  const [title, settitle] = useState("");
+  const [desc, setdesc] = useState("");
+  const [mainTask, setMainTask] = useState([])
+
+  const submithandler = (e) => {
+    e.preventDefault();
+    setMainTask([...mainTask,{title,desc}])
+    settitle("");
+    setdesc("");
+  };
+
+  const deletehandler=(i)=>{
+    let copyTask=[...mainTask]
+    copyTask.splice(i,1)
+    setMainTask(copyTask)
+  }
+  let renderTask=<h2>no Task available</h2>
+
+  if (mainTask.length>0) {
+    renderTask=mainTask.map((t,i)=>{
+      return (
+      <li key={i} className="flex justify-between items-center mb-1">
+        <div className="flex justify-between items-center mb-1 w-2/3">
+        <h5 className=" text-2xl font-semibold">{t.title}</h5>
+        <h6 className=" text-xl font-medium">{t.desc}</h6>
+      </div>
+      <button onClick={()=>{deletehandler(i)}}
+       className=" bg-red-600 text-white rounded px-1 py-2">Delete</button>
+      </li>
+      )
+    })
+  }
+  return (
+    <>
+      <div className="bg-black text-white h-[80px] text-5xl text-center uppercase pt-5">
+        my todo-list
+      </div>
+      <form
+        onSubmit={submithandler}
+        className="flex justify-evenly items-center pt-5"
+      >
+        <input
+          className="border-2 border-black  w-[400px] p-1"
+          type="text"
+          placeholder="enter title here"
+          value={title}
+          onChange={(e) => {
+            settitle(e.target.value);
+          }}
+        />
+        <input
+          className="border-2 border-black w-[400px] p-1"
+          type="text"
+          placeholder="enter description  here"
+          value={desc}
+          onChange={(e) => {
+            setdesc(e.target.value);
+          }}
+        />
+        <button className="border-2 border-black w-[100px] h-[27.2px] rounded bg-black text-white ">
+          Add task
+        </button>
+      </form>
+      
+      <div className="bg-slate-200 p-5 m-2">
+        <ul>
+          {renderTask}
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default page;
